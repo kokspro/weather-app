13f099fcf52827634d1bc93154ad7cdca0b881f8
kokspro <koks.programming@gmail.com>
2022-11-03 13:56:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 //API KEY 4fc8be2c751c49ed4a3f74c06ee95788
+
 window.addEventListener('load', ()=> {
     let long;
     let lat;
@@ -20,6 +21,6 @@ window.addEventListener('load', ()=> {
                 console.log(data);
             })
         });
-        
+        //https://api.openweathermap.org/data/2.5/weather?lat=29.760427&lon=-95.369804&appid={4fc8be2c751c49ed4a3f74c06ee95788}
     }
 });
