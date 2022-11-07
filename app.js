5d9c60627a1a45159ac497483f9bdc024cbeba95
kokspro <koks.programming@gmail.com>
2022-11-07 16:09:20 -0600

    Display JS added, search JS added, default look added

--- app.js
+++ app.js
@@ -6,7 +6,29 @@ let weatherApp = {
     fetchWeather: function (city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
         .then((response) => response.json())
-        .then((data) => console.log(data));        
+        .then((data) => this.displayWeather(data));        
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        console.log(name, icon, description, temp, humidity);
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector(".temperature").innerText = `${temp}°C`;
+        document.querySelector(".description").innerText = `${description}`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;  
+        document.querySelector(".weather").classList.remove("starting");  
+    },
+    search: function () {
+        weatherApp.fetchWeather(document.querySelector("input.search").value);
     }
-    // displayWeather
-}
+};
+
+document.querySelector(".button").addEventListener("click", weatherApp.search); 
+
+document.querySelector("input.search").addEventListener("keyup", function (event) {
+    if ( event.key == "Enter") {
+        weatherApp.search();
+    }
+});
