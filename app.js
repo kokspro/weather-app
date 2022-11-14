b68c4adfb0d62374f79e2d96d930ae457f1f4ef9
kokspro <koks.programming@gmail.com>
2022-11-13 19:35:35 -0600

    Deleted old files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 let weatherApp = {
     apiKey: "18eef0294552ee05e1f59312a308d784",
     fetchWeather: function (city) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.apiKey}`)
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));        
     },
@@ -15,7 +15,7 @@ let weatherApp = {
         console.log(name, icon, description, temp, humidity);
         document.querySelector(".city").innerText = `Weather in ${name}`;
         document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
-        document.querySelector(".temperature").innerText = `${temp}°C`;
+        document.querySelector(".temperature").innerText = `${temp}°F`;
         document.querySelector(".description").innerText = `${description}`;
         document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;  
         document.querySelector(".weather").classList.remove("starting");  
