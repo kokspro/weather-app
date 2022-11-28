2cb8a272d0e0e6506977e8eeabc59041fbedf59b
kokspro <koks.programming@gmail.com>
2022-11-27 19:09:45 -0600

    Update app.js
    Deleted old comments

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//API KEY 18eef0294552ee05e1f59312a308d784
-//https://api.openweathermap.org/data/2.5/weather?q=Houston&units=metric&appid=18eef0294552ee05e1f59312a308d784
-
 let weatherApp = {
     apiKey: "18eef0294552ee05e1f59312a308d784",
     fetchWeather: function (city) {
